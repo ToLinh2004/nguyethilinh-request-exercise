2dfeab3c281bc8718310ee531d09e2b458818099
Linh <nguyenthilinhqni2020@gmail.com>
2024-03-02 07:30:56 +0700

    Request method noitce @csrf

--- resources/views/formRegister.blade.php
+++ resources/views/formRegister.blade.php
@@ -1,6 +1,6 @@
 <form action="/postRegister" method="POST">
-    {{-- @csrf   --}}
-    {{-- tại sai caand dùng csrf --}}
+    @csrf    
+
 <pre>Name     <input type="text" name="name"></pre>
 <pre>Username <input type="text" name="userName"></pre>
 <pre>Password <input type="password" name="password"></pre>
